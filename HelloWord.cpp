04557b37090259bb71defbf680e0db3452f2116d
karoltc <karolinatrindade3@gmail.com>
2016-06-09 08:54:22 -0300

    Update HelloWord.cpp

--- HelloWord.cpp
+++ HelloWord.cpp
@@ -1,10 +1,12 @@
-//Meu primeiro código em C++.
-#include <iostream>
+//Meu primeiro cÃ³digo em C++.
+#include <iostream>//incluindo uma biblioteca 
 using namespace std;
 
 int main(int argc, char* args[])
 {
+	//imprime hello Word na tela
 	cout << "Hello Word" << endl;
-
+	
+	//retorna 0, vota para o inicio
 	return 0;
 }
